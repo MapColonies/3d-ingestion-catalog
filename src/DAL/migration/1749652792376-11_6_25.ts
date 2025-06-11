862a47c93f54922862037aa03ade5bf79ced8510
asafMasa <asafMasa@donotreplay.com>
2025-06-11 18:25:32 +0300

    chore: fix tests

--- src/DAL/migration/1749652792376-11_6_25.ts
+++ src/DAL/migration/1749652792376-11_6_25.ts
@@ -1,14 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class v2ProfileFinal1749652792376 implements MigrationInterface {
-    name = 'v2ProfileFinal1749652792376'
+  name = 'v2ProfileFinal1749652792376';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "records" ADD CONSTRAINT "UQ_product_name" UNIQUE ("product_name")`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "records" ADD CONSTRAINT "UQ_product_name" UNIQUE ("product_name")`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "records" DROP CONSTRAINT "UQ_product_name"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "records" DROP CONSTRAINT "UQ_product_name"`);
+  }
 }
